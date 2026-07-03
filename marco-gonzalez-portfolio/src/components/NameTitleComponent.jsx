3a5cd3e6a6b0d5c8a6812b0b37c52653d22cbd6f
mgonzalez12000 <43226912+mgonzalez12000@users.noreply.github.com>
2026-07-02 23:04:55 -0700

    update google cloud title

--- marco-gonzalez-portfolio/src/components/NameTitleComponent.jsx
+++ marco-gonzalez-portfolio/src/components/NameTitleComponent.jsx
@@ -5,6 +5,7 @@ const NameTitleComponent = () => {
     <div>
       <h2>Marco Gonzalez</h2>
       <h6>Software Engineer</h6>
+      <h6>Google Cloud Associate Cloud Engineer Certified</h6>
       <h6>B.S. Computer Science at California State University, Los Angeles</h6>
     </div>
   );
